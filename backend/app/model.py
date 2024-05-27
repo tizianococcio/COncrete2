965fe50d64887dc0759f292c3409e656858a8b88
Tiziano Cocciò <tiziano.coccio@gmail.com>
2024-05-27 17:04:43 +0200

    check ML model existance

--- backend/app/model.py
+++ backend/app/model.py
@@ -1,9 +1,14 @@
 import joblib
 import numpy as np
+import os
 
 def load_model():
     # Load the pre-trained model
-    model = joblib.load("../../model.pkl")
+    model_path = os.path.join(os.getcwd(), "model", "model.pkl")
+    if os.path.exists(model_path):
+        model = joblib.load(model_path)
+    else:
+        return None
     return model
 
 def predict_emissions(model, input_data):
