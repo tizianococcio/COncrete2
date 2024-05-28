ba38e3230703425154788dfdfb29c6c251f46049
Tiziano Cocciò <tiziano.coccio@gmail.com>
2024-05-28 10:35:54 +0200

    add real time co2 plot

---
+++ frontend/components/RealTimePlot.js
@@ -0,0 +1,93 @@
+import React, { useEffect, useRef, useState } from 'react';
+import { Line } from 'react-chartjs-2';
+import { Chart as ChartJS, CategoryScale, LinearScale, PointElement, LineElement, Title, Tooltip, Legend, TimeScale } from 'chart.js';
+import 'chartjs-adapter-date-fns';
+
+ChartJS.register(
+  CategoryScale,
+  LinearScale,
+  PointElement,
+  LineElement,
+  Title,
+  Tooltip,
+  Legend,
+  TimeScale
+);
+
+const RealTimePlot = ({ newEmissionData }) => {
+  const chartRef = useRef(null);
+  const [data, setData] = useState({
+    labels: [],
+    datasets: [
+      {
+        label: 'Predicted CO2 Emissions',
+        data: [],
+        fill: false,
+        borderColor: 'rgba(75,192,192,1)',
+        tension: 0.1,
+      },
+    ],
+  });
+
+  useEffect(() => {
+    if (newEmissionData !== null) {
+      const currentTime = new Date();
+      setData((prevData) => {
+        const updatedLabels = [...prevData.labels, currentTime];
+        const updatedData = [...prevData.datasets[0].data, newEmissionData];
+
+        if (updatedLabels.length > 3600) {
+          updatedLabels.shift();
+          updatedData.shift();
+        }
+
+        return {
+          ...prevData,
+          labels: updatedLabels,
+          datasets: [
+            {
+              ...prevData.datasets[0],
+              data: updatedData,
+            },
+          ],
+        };
+      });
+    }
+  }, [newEmissionData]);
+
+  return (
+    <div>
+      <Line
+        ref={chartRef}
+        data={data}
+        options={{
+          responsive: true,
+          scales: {
+            x: {
+              type: 'time',
+              time: {
+                unit: 'second',
+                tooltipFormat: 'PPpp',
+                displayFormats: {
+                  second: 'HH:mm:ss',
+                },
+              },
+              title: {
+                display: true,
+                text: 'Time (seconds)',
+              },
+            },
+            y: {
+              title: {
+                display: true,
+                text: 'Predicted CO2 Emissions',
+              },
+            },
+          },
+        }}
+      />
+    </div>
+  );
+};
+
+export default RealTimePlot;
--- frontend/components/RealTimeWebSocket.js
+++ frontend/components/RealTimeWebSocket.js
@@ -1,9 +1,11 @@
 // frontend/src/components/RealTimeWebSocket.js
 "use client";
 import React, { useEffect, useState } from 'react';
+import RealTimePlot from './RealTimePlot'
 
 const RealTimeWebSocket = ({ onTemperatureUpdate }) => {
   const [data, setData] = useState(null);
+  const [emissionData, setEmissionData] = useState(null);
 
   useEffect(() => {
     const ws = new WebSocket('ws://localhost:8001/ws');
@@ -13,6 +15,7 @@ const RealTimeWebSocket = ({ onTemperatureUpdate }) => {
       const parsed_data = JSON.parse(newData)
       onTemperatureUpdate(parsed_data.temperature);
       setData(parsed_data);
+      setEmissionData(parsed_data.predicted_co2);
     };
 
     return () => {
@@ -24,7 +27,7 @@ const RealTimeWebSocket = ({ onTemperatureUpdate }) => {
     <div className="p-4 bg-white rounded-lg shadow-md mt-4">
       <h1 className="text-2xl font-bold">Real-Time Sensor Data</h1>
       {data ? (
-        <p>
+        <div>
           <ul>
             <li>Temperature: {data.temperature.toFixed(2)} °C</li>
             <li>Humidity: {data.humidity.toFixed(2)} %</li>
@@ -38,8 +41,9 @@ const RealTimeWebSocket = ({ onTemperatureUpdate }) => {
           </ul>
           <div className="p-4 bg-slate-600 rounded-lg shadow-md m-4">
             <h3 className='text-white font-bold'>Predicted CO2 Emissions: {data.predicted_co2.toFixed(2)} kg/m³</h3>
-          </div>          
-        </p>
+          </div>         
+          <RealTimePlot newEmissionData={emissionData} /> 
+        </div>
       ) : (
         <p>Waiting for data...</p>
       )}
