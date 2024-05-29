616333364344f9ede902875373dc8596f8737ff8
Tiziano Cocciò <tiziano.coccio@gmail.com>
2024-05-29 19:18:35 +0200

    increase proxy http timeout

--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -1,13 +1,16 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    async rewrites() {
-      return [
-        {
-          source: '/api/:path*',
-          destination: 'http://backend:8000/:path*',
-        },
-      ]
-    },
-  };
+  async rewrites() {
+    return [
+      {
+        source: '/api/:path*',
+        destination: 'http://backend:8000/:path*',
+      },
+    ]
+  },
+  experimental: {
+    proxyTimeout: 360_000, /** 6 minutes for slow async requests */
+  }
+};
 
 export default nextConfig;
