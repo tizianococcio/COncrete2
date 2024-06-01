0b00f7e3033bae81baddaa6ce653f9072fb947e2
Tiziano Cocciò <tiziano.coccio@gmail.com>
2024-06-01 18:39:53 +0200

    include co2 emissions in optimization visualization

--- frontend/components/OptimalValues.js
+++ frontend/components/OptimalValues.js
@@ -26,7 +26,7 @@ const OptimalValues = ({ temperature, units }) => {
             setMessage("");
             return response;
           });
-          setOptimalValues(response.data.optimal_parameters);
+          setOptimalValues(response.data);
           setLoading(false);
         } catch (err) {
           setError(err.message);
@@ -55,17 +55,21 @@ const OptimalValues = ({ temperature, units }) => {
 
   return (
     <div className="p-4 mt-2.5 bg-white rounded-lg shadow-md">
-      <h2 className="text-2xl font-bold">Optimal Input Parameters for 1m³ of concrete (@ <span className='text-red-800'>{optimalValues.temperature.toFixed(2)} °C</span>)</h2>
+      <h2 className="text-2xl font-bold">Optimal Input Parameters for 1m³ of concrete (@ <span className='text-red-800'>{optimalValues.optimal_parameters.temperature.toFixed(2)} °C</span>)</h2>
       <p className='mb-4 text-slate-500'><div>{msg ? msg : `Next update in ${countdown} seconds...`}</div></p>
-      {optimalValues ? (
-        <ul className="space-y-1 columns-2 font-mono">
-          {Object.entries(optimalValues).map(([key, value]) => (
+      {optimalValues.optimal_parameters ? (
+        <>        <ul className="space-y-1 columns-2 font-mono">
+          {Object.entries(optimalValues.optimal_parameters).map(([key, value]) => (
             <li key={key} className="flex justify-between">
               <span>{key.replace(/_/g, ' ')}:</span>
               <span className="font-sans font-semibold text-red-800">{value.toFixed(2)} {units[key]}</span>
             </li>
           ))}
         </ul>
+        <div className="p-4 bg-slate-600 rounded-lg shadow-md m-4 text-center">
+          <h3 className='text-white'>Predicted CO2 Emissions: <span className='text-xl font-bold'>{optimalValues.expected_co2_emissions.toFixed(2)} kg/m³</span></h3>
+        </div>
+        </>
       ) : (
         <p>No optimal values found.</p>
       )}
